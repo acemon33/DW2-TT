f2bf1045ce22496bb14656fe28b8cb7b3c5704c5
acemon33 <zaxmon33@gmail.com>
2023-05-19 23:53:09 +0300

    Add: Battle-Fix Zen Recovery

--- dw2_exp_multiplier/Manager/BattleFixPatcher.cs
+++ dw2_exp_multiplier/Manager/BattleFixPatcher.cs
@@ -7,7 +7,7 @@ namespace dw2_exp_multiplier.Manager
 {
     public class BattleFixPatcher
     {
-        public static readonly string VERSION = "0.1";
+        public static readonly string VERSION = "0.2";
         
         private byte[] data;
         
@@ -19,12 +19,14 @@ namespace dw2_exp_multiplier.Manager
         public void Patch(ref FileStream fs)
         {
             this.FixVenomInfusion();
+            this.FixZenRecovery();
             PsxSector.WriteSector(ref fs, ref data, DW2Slus.GetLba(FileIndex.STAG3000_PRO), DW2Slus.GetSize(FileIndex.STAG3000_PRO));
         }
         
         public void UnPatch(ref FileStream fs)
         {
             this.UndoFixVenomInfusion();
+            this.UndoFixZenRecovery();
             PsxSector.WriteSector(ref fs, ref data, DW2Slus.GetLba(FileIndex.STAG3000_PRO), DW2Slus.GetSize(FileIndex.STAG3000_PRO));
         }
 
@@ -38,6 +40,59 @@ namespace dw2_exp_multiplier.Manager
         {
             byte[] patchedPattern =  { 0x44, 0x8E, 0x00, 0x0C };
             Buffer.BlockCopy(patchedPattern, 0, data, 0x8D38, patchedPattern.Length);    // ram: 8006c098
+        }
+        
+        private void FixZenRecovery()
+        {
+            byte[] patchedPattern =
+            {
+                0x7A, 0x03, 0x43, 0x94,
+                0x00, 0x00, 0x00, 0x00,
+                0x1C, 0x00, 0x83, 0xA6,
+                0x3E, 0x03, 0x83, 0xA6,
+                0x86, 0x03, 0x43, 0x94,
+                0x00, 0x00, 0x00, 0x00,
+                0x1E, 0x00, 0x83, 0xA6,
+                0x38, 0xD0, 0x01, 0x08,
+                0x4A, 0x03, 0x83, 0xA6
+            } ;
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x7ADC, patchedPattern.Length);    // ram: 8006ae3c
+
+            patchedPattern = new byte[]
+            {
+                0x92, 0x03, 0x42, 0x94,
+                0x00, 0x00, 0x00, 0x00,
+                0x20, 0x00, 0x82, 0xA6,
+                0x56, 0x03, 0x82, 0xA6,
+                0x98, 0xAB, 0x01, 0x08
+            };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x10D80, patchedPattern.Length);    // ram: 800740E0
+        }
+        private void UndoFixZenRecovery()
+        {
+            byte[] patchedPattern =
+            {
+                0x7A, 0x03, 0x43, 0x94,
+                0x00, 0x00, 0x00, 0x00,
+                0x1C, 0x00, 0x83, 0xA6,
+                0x86, 0x03, 0x43, 0x94,
+                0x00, 0x00, 0x00, 0x00,
+                0x1E, 0x00, 0x83, 0xA6,
+                0x92, 0x03, 0x42, 0x94,
+                0x00, 0x00, 0x00, 0x00,
+                0x20, 0x00, 0x82, 0xA6
+            } ;
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x7ADC, patchedPattern.Length);    // ram: 8006ae3c
+            
+            patchedPattern = new byte[]
+            {
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00
+            };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x10D80, patchedPattern.Length);    // ram: 800740E0
         }
     }
     
