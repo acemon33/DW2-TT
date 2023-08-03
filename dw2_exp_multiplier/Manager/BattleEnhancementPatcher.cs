56a981d842338c38d2f770f6b4fd12cd8beb3270
acemon33 <zaxmon33@gmail.com>
2023-08-03 20:50:43 +0300

    Add: 3 Battle-Enhancement Patchers

--- dw2_exp_multiplier/Manager/BattleEnhancementPatcher.cs
+++ dw2_exp_multiplier/Manager/BattleEnhancementPatcher.cs
@@ -7,11 +7,14 @@ namespace dw2_exp_multiplier.Manager
 {
     public class BattleEnhancementPatcher
     {
-        public static readonly string VERSION = "0.3";
+        public static readonly string VERSION = "0.6";
         public static readonly string TOOLTIP =
             "1. MP-on-Guard: ratio of increasing MP during Guard is 25%\n" +
-            "2. Beast King Fist Tech: the damage is 2.1 on Counter-Attack\n" +
-            "3. Subzero Ice Punch: every hit increases 7 AP\n";
+            "2. Beast King Fist: the damage is 2.1 on Counter-Attack\n" +
+            "3. Subzero Ice Punch: every hit increases 7 AP\n" +
+            "4. Demi Dart and Evil Touch: Lowering MP increased\n" +
+            "5. Poison Effect Damage: Poison Damage increased\n" +
+            "6. Debuff Defence Effect: Reduce Defence after the damage\n";
 
         private byte[] data;
         
@@ -25,6 +28,9 @@ namespace dw2_exp_multiplier.Manager
             this.ApplyMpOnGuard();
             this.ApplyBeastKingFist();
             this.ApplySubzeroIcePunch();
+            this.ApplyMpReduction();
+            this.ApplyPoisonDamage();
+            this.ApplyDebuffDefence();
             PsxSector.WriteSector(ref fs, ref data, DW2Slus.GetLba(FileIndex.STAG3000_PRO), DW2Slus.GetSize(FileIndex.STAG3000_PRO));
         }
         
@@ -106,6 +112,61 @@ namespace dw2_exp_multiplier.Manager
         {
             byte[] patchedPattern = { 0x05, 0x00, 0x62, 0x24 };
             Buffer.BlockCopy(patchedPattern, 0, data, 0x7E48, patchedPattern.Length);    // ram: 8006b1a8
+        }
+
+        private void ApplyMpReduction()
+        {
+            byte[] patchedPattern = { 0x08, 0x00, 0x42, 0x30, 0x14, 0x00, 0x42, 0x20 };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x84D8, patchedPattern.Length);    // ram: 8006b838
+
+            patchedPattern = new byte[] { 0x15, 0x00, 0x42, 0x30, 0x32, 0x00, 0x42, 0x24 };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x8524, patchedPattern.Length);    // ram: 8006b884
+        }
+
+        private void ApplyPoisonDamage()
+        {
+            byte[] patchedPattern = { 0x11, 0x00, 0x52, 0x26 };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x8218, patchedPattern.Length);    // ram: 8006b578
+        }
+
+        private void ApplyDebuffDefence()
+        {
+            byte[] patchedPattern = 
+            {
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00
+            };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x79C8, patchedPattern.Length);    // ram: 8006ad28
+
+            patchedPattern = new byte[] { 0x4C, 0xD0, 0x01, 0x08, 0x00, 0x00, 0x00, 0x00 };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x8208, patchedPattern.Length);    // ram: 8006b568
+
+            patchedPattern = new byte[]
+            {
+                0x1A, 0x00, 0x24, 0x01,
+                0x20, 0x48, 0x02, 0x00,
+                0x60, 0x00, 0xA4, 0x8F,
+                0x4C, 0x7C, 0x00, 0x0C,
+                0x00, 0x00, 0x00, 0x00,
+                0x04, 0x00, 0x42, 0x30,
+                0x09, 0x00, 0x40, 0x10,
+                0x00, 0x00, 0x00, 0x00,
+                0x40, 0x10, 0x16, 0x00,
+                0x07, 0x80, 0x12, 0x3C,
+                0x46, 0x40, 0x52, 0x26,
+                0x21, 0x20, 0x52, 0x00,
+                0x1E, 0x00, 0x85, 0x26,
+                0xDC, 0xFF, 0x46, 0x26,
+                0x5A, 0xAA, 0x01, 0x0C,
+                0x21, 0x30, 0x46, 0x00,
+                0x12, 0x90, 0x00, 0x00,
+                0x5C, 0xAD, 0x01, 0x08,
+                0x20, 0x10, 0x09, 0x00
+            };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x10DD0, patchedPattern.Length);    // ram: 80074130
         }
     }
     
