3d4c370ee402306b90a6f6c73899ec3481d149f8
acemon33 <zaxmon33@gmail.com>
2024-10-04 21:54:00 +0300

    Add: Battle Enhancement Patchers

---
+++ dw2_exp_multiplier/Manager/BattleEnhancement/MpOnGuardPatcher.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using dw2_exp_multiplier.Base;
+
+
+namespace dw2_exp_multiplier.Manager
+{
+    public class MpOnGaurdPatcher : IPatcher
+    {
+        public static readonly string TOOLTIP = ".\n";
+
+        private byte[] data;
+
+        public override void Patch(ref FileStream fs)
+        {
+            data = PsxSector.ReadSector(ref fs, DW2Slus.GetLba(FileIndex.STAG3000_PRO), DW2Slus.GetSize(FileIndex.STAG3000_PRO));
+
+            byte[] patchedPattern =
+            {
+                0x04, 0x00, 0x05, 0x24,
+                0x1B, 0x00, 0x85, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x12, 0x18, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x32, 0x00, 0xC2, 0x94,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00
+            };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x9774, patchedPattern.Length);
+
+            PsxSector.WriteSector(ref fs, ref data, DW2Slus.GetLba(FileIndex.STAG3000_PRO), DW2Slus.GetSize(FileIndex.STAG3000_PRO));
+        }
+    }
+    
+}
