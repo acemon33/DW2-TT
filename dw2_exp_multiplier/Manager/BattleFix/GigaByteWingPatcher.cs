c321e009bf915dc3f7fb71a31d4f5fa01c99573c
acemon33 <zaxmon33@gmail.com>
2024-10-04 21:53:26 +0300

    Add: Battle Fix Patchers

---
+++ dw2_exp_multiplier/Manager/BattleFix/GigaByteWingPatcher.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using dw2_exp_multiplier.Base;
+
+
+namespace dw2_exp_multiplier.Manager
+{
+    public class GigaByteWingPatcher : IPatcher
+    {
+        public static readonly string TOOLTIP = ".\n";
+
+        private byte[] data;
+
+        public override void Patch(ref FileStream fs)
+        {
+            data = PsxSector.ReadSector(ref fs, DW2Slus.GetLba(FileIndex.STAG3000_PRO), DW2Slus.GetSize(FileIndex.STAG3000_PRO));
+
+            byte[] patchedPattern =
+            {
+                0x07, 0x80, 0x02, 0x3C,
+                0xB2, 0xD0, 0x01, 0x08,
+                0xC0, 0x3C, 0x50, 0x24
+            };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x6730, patchedPattern.Length);
+
+            patchedPattern = new byte[]
+            {
+                0x21, 0x10, 0x90, 0x02,
+                0x4F, 0x03, 0x42, 0x90,
+                0xB8, 0x31, 0x90, 0x24,
+                0x04, 0x00, 0x42, 0x30,
+                0x03, 0x00, 0x40, 0x14,
+                0x07, 0x80, 0x02, 0x3C,
+                0xA7, 0xA6, 0x01, 0x08,
+                0xC8, 0x31, 0x51, 0x24,
+                0xEB, 0xA6, 0x01, 0x08,
+                0x00, 0x00, 0x00, 0x00
+            };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x10F68, patchedPattern.Length);
+
+            patchedPattern = new byte[]
+            {
+                0x9C, 0xD0, 0x01, 0x08,
+                0x2E, 0x00, 0x43, 0x94
+            };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x7550, patchedPattern.Length);
+
+            patchedPattern = new byte[]
+            {
+                0x00, 0x00, 0x00, 0x00,
+                0x02, 0x00, 0x76, 0x28,
+                0x03, 0x00, 0xC0, 0x12,
+                0x2C, 0x00, 0x43, 0x94,
+                0x2E, 0xAA, 0x01, 0x08,
+                0x01, 0x00, 0x16, 0x34,
+                0x33, 0xAA, 0x01, 0x08,
+                0x00, 0x00, 0x00, 0x00
+            };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x10F10, patchedPattern.Length);
+
+            PsxSector.WriteSector(ref fs, ref data, DW2Slus.GetLba(FileIndex.STAG3000_PRO), DW2Slus.GetSize(FileIndex.STAG3000_PRO));
+        }
+    }
+    
+}
---
+++ dw2_exp_multiplier/Manager/BattleFix/InvincibilityPatcher .cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using dw2_exp_multiplier.Base;
+
+
+namespace dw2_exp_multiplier.Manager
+{
+    public class InvincibilityPatcher : IPatcher
+    {
+        public static readonly string TOOLTIP = ".\n";
+
+        private byte[] data;
+
+        public override void Patch(ref FileStream fs)
+        {
+            data = PsxSector.ReadSector(ref fs, DW2Slus.GetLba(FileIndex.STAG3000_PRO), DW2Slus.GetSize(FileIndex.STAG3000_PRO));
+
+            byte[] patchedPattern = { 0x91, 0xD0, 0x01, 0x08, 0x07, 0x80, 0x02, 0x3C };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x7CB8, patchedPattern.Length);
+
+            patchedPattern = new byte[]
+            {
+                0xC0, 0x3C, 0x43, 0x24,
+                0x21, 0x10, 0xC3, 0x02,
+                0x4F, 0x03, 0x43, 0x90,
+                0x00, 0x00, 0x00, 0x00,
+                0x08, 0x00, 0x62, 0x30,
+                0x03, 0x00, 0x40, 0x14,
+                0xB8, 0x02, 0x82, 0x94,
+                0x08, 0xAC, 0x01, 0x08,
+                0x16, 0x00, 0x83, 0x86,
+                0x14, 0xAC, 0x01, 0x08,
+                0x00, 0x00, 0x00, 0x00
+            };
+            Buffer.BlockCopy(patchedPattern, 0, data, 0x10EE4, patchedPattern.Length);
+
+            PsxSector.WriteSector(ref fs, ref data, DW2Slus.GetLba(FileIndex.STAG3000_PRO), DW2Slus.GetSize(FileIndex.STAG3000_PRO));
+        }
+    }
+    
+}
